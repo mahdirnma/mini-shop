8be5dc7e929d33a400af966db0a92148b397dcd9
mahdirnma <rahnamamahdi78@gmail.com>
2025-01-27 23:19:34 +0330

    add tailwind to project and create login and register panel

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function loginForm(){
+        return view('user.login');
+    }
+    public function registerForm(){
+        return view('user.register');
+    }
 }
