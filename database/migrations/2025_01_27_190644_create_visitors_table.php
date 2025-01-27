07b3649a19542b78b11cc713985d1423c530fa44
mahdirnma <rahnamamahdi78@gmail.com>
2025-01-27 22:41:31 +0330

    fill migrations

--- database/migrations/2025_01_27_190644_create_visitors_table.php
+++ database/migrations/2025_01_27_190644_create_visitors_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('visitors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('age');
+            $table->string('gender');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
--- database/migrations/2025_01_27_190658_create_products_table.php
+++ database/migrations/2025_01_27_190658_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('price');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
